e35d011845f398807e87d2f9c319900749fc8977
vanyoooo <vanyoo@yahoo.com>
2026-04-03 00:47:07 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+async function trainModel() {
+  const output = document.getElementById("output");
+  output.innerText = "Обучаване... ⏳";
+
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+  model.compile({
+    loss: 'meanSquaredError',
+    optimizer: 'sgd'
+  });
+
+  const xs = tf.tensor([1, 2, 3, 4], [4, 1]);
+  const ys = tf.tensor([1, 3, 5, 7], [4, 1]);
+
+  await model.fit(xs, ys, { epochs: 200 });
+
+  const prediction = model.predict(tf.tensor([5], [1, 1]));
+  const result = prediction.dataSync()[0];
+
+  output.innerText = "Прогноза за x=5: " + result.toFixed(2);
+}
